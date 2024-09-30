ff26a1e4164478b710a0ba79b8f980a6b789b1a0
Luis Gonzalez Ruiz (CM) <lgonzalezr@copaair.com>
2024-09-30 17:46:58 -0500

    Se agregaron los DbSets de IrregularOperations, Groups, IrregularOperationsLogs. Ademas se agregaron los modelos respectivos tanto Domain como Common

---
+++ CrewMobile.Api/CrewMobile.Common/Models/GroupCom.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrewMobile.Common.Models
+{
+    public class GroupCom
+    {
+        [Display(Name = "Group Id")]
+        public string GroupGuid { get; set; }
+    }
+}
---
+++ CrewMobile.Api/CrewMobile.Domain/Models/IrregularOperationsLog.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CrewMobile.Common.Models;
+
+namespace CrewMobile.Domain.Models
+{
+    public class IrregularOperationsLog : IrregularOperationsLogCom
+    {
+        [Key]
+        public int IrregularOperationsLogId { get; set; }
+    }
+}
